a7f4360f4157e1264d37b880cf49ece587197970
Burandelei <92280548+dev-Burandelei@users.noreply.github.com>
2023-08-31 14:23:33 -0300

    V_2.0

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
-
-import THREE, {
-  CircleGeometry,
-  BoxGeometry,
-  DirectionalLight,
-  Mesh,
-  MeshPhongMaterial,
+import {
+  ArrowHelper,
+  AxesHelper,
+  Color,
+  HemisphereLight,
+  Object3D,
+  Raycaster,
   Scene,
+  Vector3,
 } from "three"
+
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js"
 
 import { Camera } from "./core/Camera"
 import { Renderer } from "./core/Renderer"
-import { Satellite } from "./core/satelite";
 
+import { PlayerCamera } from "./core/PlayerCamera"
+import { BulbLight } from "./lights/BulbLight"
+import { MoonLight } from "./lights/MoonLight"
+
+import { Bedroom } from "./models/Bedroom"
 
 // Get main canvas element
 const canvas = document.getElementById("app") as HTMLCanvasElement
@@ -20,58 +27,98 @@ const canvas = document.getElementById("app") as HTMLCanvasElement
 const renderer = new Renderer(canvas)
 const mainCamera = new Camera()
 
+const objects: Object3D[] = []
+
 // Create scene
 const scene = new Scene()
+scene.background = new Color("#080820")
 
-// Create cube geometry and material
-const geometry = new CircleGeometry()
-const geometry2 = new BoxGeometry()
+// Create raycaster
+const raycaster = new Raycaster(new Vector3(), new Vector3(0, 0, 1), 0, 30)
+const raycasterHelper = new ArrowHelper(
+  raycaster.ray.direction,
+  raycaster.ray.origin,
+  300,
+  0xff0000
+)
 
-const material = new MeshPhongMaterial({ color: 0xffffff })
+scene.add(raycasterHelper)
 
-// Create cube mesh and set position
-const circle = new Mesh(geometry, material)
-const box = new Mesh(geometry2, material)
-circle.position.z = -5
-circle.position.x = 0
+// Create pointer lock controls
+const pointerControl = new PointerLockControls(mainCamera, document.body)
+scene.add(pointerControl.getObject())
 
-box.position.z = -4
-box.position.x = 10
+// Add axes helper
+scene.add(new AxesHelper(500))
 
+// Create hemisphere light
+const hemiLight = new HemisphereLight("#29294d", "#080820", 2)
+scene.add(hemiLight)
 
-// Add cube to scene
-//scene.add(box)
-//scene.add(circle)
-  
+// Create a moon light
+const moonLight = new MoonLight({ x: -5, y: 3, z: -3 })
+scene.add(moonLight, moonLight.helper)
 
-//add satelite to scene
+// Create bulb light
+const bulbLight = new BulbLight({ x: 1.47, y: 0.75, z: -1.3 })
+scene.add(bulbLight, bulbLight.helper)
 
-const satellite = new Satellite();
+// Load scenario model
+const bedroom = await Bedroom.object()
+scene.add(bedroom)
 
-satellite.getMesh().position.z = -4
-satellite.getMesh().position.x = -1
-scene.add(satellite.getMesh());
+// Create first person camera
+const playerCamera = new PlayerCamera(mainCamera, canvas)
 
-// Create directional light, set color, intensity and position
-const light = new DirectionalLight(0xffffff, 1)
-light.position.set(0, 4, 2)
+// Start rendering
+let previousFrameTime: number | null = null
 
-// Add light to scene
-scene.add(light)
-
-// Main function to render the scene every time a request to canvas is made
-let angle = 0
-mainCamera.position.z = -5;
-function process() {
-  
-  mainCamera.position.x = 12 * Math.sin(angle);
-  mainCamera.position.z = 12 * Math.cos(angle);
-  mainCamera.lookAt(satellite.getMesh().position);  // Faz a câmera sempre olhar para o cubo
-
-  angle += 0.01;  // Incrementa o ângulo para a rotação
-  renderer.render(scene, mainCamera)
-  requestAnimationFrame(process)
-}
-
+objects.push(bedroom)
 
 process()
+
+// Main function to render the scene every time a request to canvas is made
+function process() {
+  requestAnimationFrame((t) => {
+    if (previousFrameTime === null) previousFrameTime = t
+
+    animate(t - previousFrameTime)
+
+    renderer.autoClear = true
+    renderer.render(scene, mainCamera)
+    renderer.autoClear = false
+
+    previousFrameTime = t
+
+    pointerControl.lock()
+
+    process()
+  })
+}
+
+function animate(timeElapsed: number) {
+  const timeElapsedS = timeElapsed * 0.001
+
+  if (pointerControl.isLocked) {
+    raycaster.ray.origin.copy(pointerControl.getObject().position)
+    raycaster.ray.origin.y = 1
+
+    const intersections = raycaster.intersectObjects(objects, true)
+    // console.log(intersections)
+    const onObject = intersections.length > 0
+
+    const delta = timeElapsed
+
+    if (onObject === true) {
+      intersections.forEach((intersection) => {
+        const object = intersection.object
+
+        if (object.name !== "") {
+          playerCamera.movementSpeed = Math.max(0, 0)
+        }
+      })
+    }
+  }
+
+  playerCamera.update_(timeElapsedS)
+}
